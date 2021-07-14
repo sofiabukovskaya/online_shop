00fa87c981335451f4636a9e96fac5d278bf7a48
Sofia Bukovskaya <69693280+sofiabukovskaya@users.noreply.github.com>
2021-07-14 17:44:53 +0300

    Added a blue color circle

--- lib/screens/details/widgets/details_body.dart
+++ lib/screens/details/widgets/details_body.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_online_shop/constants.dart';
 import 'package:flutter_online_shop/models/product.dart';
@@ -20,12 +21,45 @@ class DetailsBody extends StatelessWidget {
               children: [
                 Container(
                   margin: EdgeInsets.only(top: size.height * 0.3),
-                  height: 500,
+                  padding: EdgeInsets.only(
+                      top: size.height * 0.12,
+                      left: kDefaultPadding,
+                      right: kDefaultPadding),
                   decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(24),
                           topRight: Radius.circular(24))),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Column(
+                            children: [
+                              Text("Color"),
+                              Container(
+                                margin: EdgeInsets.only(
+                                    top: kDefaultPadding / 4,
+                                    right: kDefaultPadding / 2),
+                                padding: EdgeInsets.all(2.5),
+                                height: 24,
+                                width: 24,
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    border:
+                                        Border.all(color: Color(0xFF356C95))),
+                                child:
+                                    DecoratedBox(decoration: BoxDecoration(
+                                      color: Color(0xFF356C95),
+                                      shape: BoxShape.circle
+                                    )),
+                              )
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
                 ProductTitleWithImage(product: product)
               ],
